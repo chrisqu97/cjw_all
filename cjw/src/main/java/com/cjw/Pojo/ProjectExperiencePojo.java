10de447fedcc41dfd9c99d679bb8a2eae9f41d61
chrisqu <392788234@qq.com>
2018-09-17 15:07:53 +0800

    增加字段

--- cjw/src/main/java/com/cjw/Pojo/ProjectExperiencePojo.java
+++ cjw/src/main/java/com/cjw/Pojo/ProjectExperiencePojo.java
@@ -4,4 +4,5 @@ import lombok.Data;
 
 @Data
 public class ProjectExperiencePojo {
+    private String content;
 }
--- cjw/src/main/java/com/cjw/Pojo/WorkExperiencePojo.java
+++ cjw/src/main/java/com/cjw/Pojo/WorkExperiencePojo.java
@@ -8,4 +8,5 @@ import lombok.Data;
  */
 @Data
 public class WorkExperiencePojo {
+    private String content;
 }
