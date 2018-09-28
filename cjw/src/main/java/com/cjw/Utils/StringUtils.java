483720fd43743190e753b2a9c22cb1c482540682
chrisqu97 <35282752+chrisqu97@users.noreply.github.com>
2018-09-28 16:53:16 +0800

    Delete StringUtils.java

--- cjw/src/main/java/com/cjw/Utils/StringUtils.java
+++
@@ -1,7 +0,0 @@
-package com.cjw.utils;
-
-public class StringUtils extends org.springframework.util.StringUtils {
-    public static boolean isNotEmpty(Object o) {
-        return !isEmpty(o);
-    }
-}
