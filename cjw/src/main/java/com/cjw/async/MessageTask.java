64058caf01d583dbb4cdc0a192d12aef94805389
chrisqu <392788234@qq.com>
2018-11-08 19:10:54 +0800

    标记未读

--- cjw/src/main/java/com/cjw/async/MessageTask.java
+++ cjw/src/main/java/com/cjw/async/MessageTask.java
@@ -35,6 +35,7 @@ public class MessageTask {
     public MessagePojo convertToMessagePojo(String message) {
         MessagePojo messagePojo = JSON.parseObject(message, MessagePojo.class);
         messagePojo.setCreateTime(System.currentTimeMillis() + "");
+        messagePojo.setIfRead(1);
         return messagePojo;
     }
 
